523dd49743edc5c95df8ec0fe0a00fca9c067284
mazarimono <32221063+mazarimono@users.noreply.github.com>
2021-04-16 17:29:09 +0900

    add server = app.server

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ graph_types = [px.line, px.scatter, px.bar]
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(
     [
